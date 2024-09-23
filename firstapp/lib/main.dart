8826021ad971cc948d9c4c5992f92a5d815c6c53
Nitram099 <nitrammato@gmail.com>
2024-09-23 20:57:29 +0300

    week 5 assignment

---
+++ firstapp/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My First Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to Flutter!',
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Button pressed!');
+              },
+              child: const Text('Click Me'),
+            ),
+            Image.network(
+              'https://picsum.photos/250/250',
+              width: 250,
+              height: 250,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
